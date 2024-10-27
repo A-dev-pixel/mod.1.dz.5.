0c803d91e6a0989e9842b38545044e551530c4c9
A-dev-pixel <podlesnovav91@gmail.com>
2024-10-27 18:59:36 +0300

    Add files via upload

---
+++ mod.1.dz5.py
@@ -0,0 +1,9 @@
+#module_1_5.py
+mmutable_var=(1,2,"false")
+print(mmutable_var)
+# mmutable_var[0]=5
+# print(mmutable_var)
+mutable_list=["apple", "banana", 1.2]
+print(mutable_list)
+mutable_list[0]=1
+print(mutable_list)
